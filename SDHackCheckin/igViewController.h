1666c1576c296f7b2dd6bde1dca4647990f99387
Li-Wei Tseng <li-weitseng@Li-Weis-MacBook-Pro.local>
2015-10-03 01:41:03 -0700

    add Parse

--- SDHackCheckin/igViewController.h
+++ SDHackCheckin/igViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/PFQuery.h>
 
 @interface igViewController : UIViewController
 
--- SDHackCheckin/igViewController.m
+++ SDHackCheckin/igViewController.m
@@ -6,19 +6,21 @@
 //  Copyright (c) 2013 Infragistics. All rights reserved.
 //
 
+#import "ConfirmationViewController.h"
 #import <AVFoundation/AVFoundation.h>
 #import "igViewController.h"
 
-@interface igViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@interface igViewController () <AVCaptureMetadataOutputObjectsDelegate,UIAlertViewDelegate>
 {
     AVCaptureSession *_session;
     AVCaptureDevice *_device;
     AVCaptureDeviceInput *_input;
     AVCaptureMetadataOutput *_output;
     AVCaptureVideoPreviewLayer *_prevLayer;
-
+    PFObject *_student;
     UIView *_highlightView;
     UILabel *_label;
+    CGRect highlightViewRect;
 }
 @end
 
@@ -32,6 +34,8 @@
     _highlightView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin;
     _highlightView.layer.borderColor = [UIColor greenColor].CGColor;
     _highlightView.layer.borderWidth = 3;
+    highlightViewRect = CGRectMake(38, 271, 269, 80);
+    _highlightView.frame = highlightViewRect;
     [self.view addSubview:_highlightView];
 
     _label = [[UILabel alloc] init];
@@ -40,42 +44,47 @@
     _label.backgroundColor = [UIColor colorWithWhite:0.15 alpha:0.65];
     _label.textColor = [UIColor whiteColor];
     _label.textAlignment = NSTextAlignmentCenter;
-    _label.text = @"Scanning";
     [self.view addSubview:_label];
+    [self startScanning];
+    }
 
+- (void)startScanning
+{
+    _label.text = @"Scanning";
     _session = [[AVCaptureSession alloc] init];
     _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     NSError *error = nil;
-
+    
     _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
     if (_input) {
         [_session addInput:_input];
     } else {
         NSLog(@"Error: %@", error);
     }
-
+    
     _output = [[AVCaptureMetadataOutput alloc] init];
     [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
     [_session addOutput:_output];
-
+    
     _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
-
+    
     _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
     _prevLayer.frame = self.view.bounds;
     _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
     [self.view.layer addSublayer:_prevLayer];
-
+    
     [_session startRunning];
-
+    
     [self.view bringSubviewToFront:_highlightView];
     [self.view bringSubviewToFront:_label];
+
 }
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
 {
-    CGRect highlightViewRect = CGRectZero;
     AVMetadataMachineReadableCodeObject *barCodeObject;
     NSString *detectionString = nil;
+    NSString *studentName = nil;
     NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
             AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
             AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
@@ -93,14 +102,63 @@
 
         if (detectionString != nil)
         {
-            _label.text = detectionString;
+            NSString *correctID = [detectionString substringToIndex:[detectionString length]-1];
+            PFQuery *query = [PFQuery queryWithClassName:@"student"];
+            [query whereKey:@"studentID" equalTo:correctID];
+            NSArray *foundObjects = [query findObjects];
+            if ([foundObjects count]) {
+                _label.text = @"Scanned";
+                NSLog(@" data %@",[query findObjects]);
+                _student = [[query findObjects] firstObject];
+                NSLog(@" data %@",_student);
+                NSLog(@" data %@",_student[@"fname"]);
+                studentName = [_student[@"fname"] stringByAppendingString:@" "];
+                studentName = [studentName stringByAppendingString:_student[@"lname"]];
+                [_session stopRunning];
+            }
             break;
         }
         else
             _label.text = @"Scanning";
     }
+    
+    if (studentName) {
+        [[[UIAlertView alloc] initWithTitle:@"Is this the person you want to check in?"
+                                    message:studentName
+                                   delegate:self
+                          cancelButtonTitle:nil
+                          otherButtonTitles:@"Yes", @"Retry",nil] show];
+    }
+    
+}
 
-    _highlightView.frame = highlightViewRect;
+- (void) alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    switch (buttonIndex) {
+        case 0:
+            NSLog(@"YES Pressed");
+            _label.text = @"Scanning";
+            [self updateCheckin];
+            [_session startRunning];
+            break;
+        case 1:
+            NSLog(@"RETRY Pressed");
+            _label.text = @"Scanning";
+            [_session startRunning];
+            break;
+        default:
+            break;
+    }
+}
+
+- (void)updateCheckin
+{
+    PFQuery *query = [PFQuery queryWithClassName:@"student"];
+    [query getObjectInBackgroundWithId:_student.objectId
+                                 block:^(PFObject *student, NSError *error) {
+                                     [student incrementKey:@"Checkin"];
+                                     [student saveInBackground];
+                                 }];
 }
 
 @end
