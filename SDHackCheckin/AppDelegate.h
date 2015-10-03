822fbd6d58ef7179acb8612550e729e8e29d80d8
LI-WEI TSENG <tsengliwei@gmail.com>
2015-10-02 21:33:21 -0700

    first commit

---
+++ SDHackCheckin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDHackCheckin
+//
+//  Created by Li-Wei Tseng on 10/2/15.
+//  Copyright © 2015 liwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDHackCheckin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDHackCheckin
+//
+//  Created by Li-Wei Tseng on 10/2/15.
+//  Copyright © 2015 liwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
