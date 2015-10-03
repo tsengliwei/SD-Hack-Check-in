979f3d6a86cfc6371b06e4b87b1bd44a352ca528
Li-Wei Tseng <li-weitseng@Li-Weis-MacBook-Pro.local>
2015-10-02 23:40:54 -0700

    add qr scanner

---
+++ SDHackCheckin/ConfirmationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConfirmationViewController.h
+//  SDHackCheckin
+//
+//  Created by Li-Wei Tseng on 10/2/15.
+//  Copyright © 2015 liwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ConfirmationViewController : ViewController
+
+@end
--- SDHackCheckin/ViewController.m
+++ SDHackCheckin/ViewController.m
@@ -7,91 +7,21 @@
 //
 
 #import "ViewController.h"
-#import "MTBBarcodeScanner.h"
 #import <Parse/Parse.h>
 
 @interface ViewController ()
-@property (nonatomic, strong) MTBBarcodeScanner *scanner;
-@property (nonatomic, strong) NSMutableArray *uniqueCodes;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)scanTapped:(id)sender {
-    if ([self.scanner isScanning]) {
-        [self stopScanning];
-    } else {
-        [MTBBarcodeScanner requestCameraPermissionWithSuccess:^(BOOL success) {
-            if (success) {
-                [self startScanning];
-            } else {
-                [self displayPermissionMissingAlert];
-            }
-        }];
-    }
-
-}
-
-#pragma mark - Scanner
-
-- (MTBBarcodeScanner *)scanner {
-    if (!_scanner) {
-        _scanner = [[MTBBarcodeScanner alloc] initWithPreviewView:self.view];
-    }
-    return _scanner;
-}
-
-#pragma mark - Scanning
-
-- (void)startScanning {
-    self.uniqueCodes = [[NSMutableArray alloc] init];
-    
-    [self.scanner startScanningWithResultBlock:^(NSArray *codes) {
-        for (AVMetadataMachineReadableCodeObject *code in codes) {
-            if (code.stringValue && [self.uniqueCodes indexOfObject:code.stringValue] == NSNotFound) {
-                [self.uniqueCodes addObject:code.stringValue];
-                
-                NSLog(@"Found unique code: %@", code.stringValue);
-                
-            }
-        }
-    }];
-}
-
-- (void)stopScanning {
-    [self.scanner stopScanning];
-}
-
-#pragma mark - Helper Methods
-
-- (void)displayPermissionMissingAlert {
-    NSString *message = nil;
-    if ([MTBBarcodeScanner scanningIsProhibited]) {
-        message = @"This app does not have permission to use the camera.";
-    } else if (![MTBBarcodeScanner cameraIsPresent]) {
-        message = @"This device does not have a camera.";
-    } else {
-        message = @"An unknown error occurred.";
-    }
-    
-    [[[UIAlertView alloc] initWithTitle:@"Scanning Unavailable"
-                                message:message
-                               delegate:nil
-                      cancelButtonTitle:@"Ok"
-                      otherButtonTitles:nil] show];
 }
 
 @end
